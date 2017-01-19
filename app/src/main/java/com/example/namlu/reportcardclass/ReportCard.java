0f95b97c2f552bed5d70cdf74474e58ccee0ed83
namclu <namlu1205@gmail.com>
2017-01-19 21:13:08 +1100

    1 Add instance variables for gradeArt, gradeHistory, gradeMath, and gradeScience, and getters and setters. Update toString() to output new variables. MainActivity: update call of newReportCard

--- app/src/main/java/com/example/namlu/reportcardclass/ReportCard.java
+++ app/src/main/java/com/example/namlu/reportcardclass/ReportCard.java
@@ -1,7 +1,5 @@
 package com.example.namlu.reportcardclass;
 
-import java.util.Arrays;
-
 /**
  * Created by namlu on 19-Jan-17.
  *
@@ -13,19 +11,102 @@ public class ReportCard {
     public static final String TAG = "REPORT CARD";
     private final String SCHOOL_NAME = "Udacity University";
 
-    private String[] schoolSubjects = new String[]{"Art", "History", "Math, Science", "Social Studies"};
+    // Array of school subjects
+    private String[] schoolSubjects = new String[]{"Art", "History", "Math", "Science"};
+
+    // Instance variables
+    private String studentName;
+
+    // Grades are values between 0 - 100
+    private int gradeArt;
+    private int gradeHistory;
+    private int gradeMath;
+    private int gradeScience;
 
     // Class constructor
-    public ReportCard() {
-        this.schoolSubjects = schoolSubjects;
+    public ReportCard(
+            String studentName,
+            int gradeArt,
+            int gradeHistory,
+            int gradeMath,
+            int gradeScience) {
+        setStudentName(studentName);
+        setGradeArt(gradeArt);
+        setGradeHistory(gradeHistory);
+        setGradeMath(gradeMath);
+        setGradeScience(gradeScience);
     }
 
     @Override
     public String toString() {
+
         return "ReportCard{" +
-                "TAG = '" + TAG + '\'' +
+                "\n TAG = '" + TAG + '\'' +
                 "\n SCHOOL_NAME = '" + SCHOOL_NAME + '\'' +
-                "\n schoolSubjects = " + Arrays.toString(schoolSubjects) +
+                "\n studentName = " + getStudentName() +
+                "\n" + schoolSubjects[0] + " = " + getGradeArt() +
+                "\n" + schoolSubjects[1] + " = " + getGradeHistory() +
+                "\n" + schoolSubjects[2] + " = " + getGradeMath() +
+                "\n" + schoolSubjects[3] + " = " + getGradeScience() +
                 '}';
+    }
+
+    /*
+     * Getter and setter methods for ReportCard
+     */
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        if (studentName != null) {
+            this.studentName = studentName;
+        } else
+            studentName = "No name given";
+    }
+
+    public int getGradeArt() {
+        return gradeArt;
+    }
+
+    public void setGradeArt(int gradeArt) {
+        if (gradeArt >= 0 && gradeArt <=100) {
+            this.gradeArt = gradeArt;
+        }else
+            gradeArt = 0;
+    }
+
+    public int getGradeHistory() {
+        return gradeHistory;
+    }
+
+    public void setGradeHistory(int gradeHistory) {
+        if (gradeHistory >= 0 && gradeHistory <=100) {
+            this.gradeHistory = gradeHistory;
+        } else
+            gradeHistory = 0;
+    }
+
+    public int getGradeMath() {
+        return gradeMath;
+    }
+
+    public void setGradeMath(int gradeMath) {
+        if (gradeMath >= 0 && gradeMath <=100) {
+            this.gradeMath = gradeMath;
+        } else
+            gradeMath = 0;
+    }
+
+    public int getGradeScience() {
+        return gradeScience;
+    }
+
+    public void setGradeScience(int gradeScience) {
+        if (gradeScience >= 0 && gradeScience <=100) {
+            this.gradeScience = gradeScience;
+        } else
+            gradeScience = 0;
     }
 }
